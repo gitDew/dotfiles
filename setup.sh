7af3705169fe6aa74cf74f39e160ae04e1943127
gitDew <krisztian.harmat@gmail.com>
2021-02-15 04:19:24 +0100

    Fix something that might not be broken

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ function linkDotfile {
 linkDotfile .vimrc
 linkDotfile .tmux.conf
 
-mkdir -p $dotfilesDir/.vim/bundle
-cd $dotfilesDir/.vim/bundle
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
 git clone git://github.com/VundleVim/Vundle.vim.git
 vim +PluginInstall +qall
