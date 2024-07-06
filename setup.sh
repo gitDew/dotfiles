2bab32c3222322fdee608d701b84ae8e46cc4dc5
Krisztian Harmat <krisztian.harmat@gmail.com>
2024-07-06 18:43:52 +0200

    Autoinstall

--- setup.sh
+++ setup.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 dotfilesDir=$(pwd)
 
-function linkDotfile {
-  dest="${HOME}/${1}"
-  dateStr=$(date +%Y-%m-%d-%H%M)
+sudo apt install curl
+sudo apt install neovim
 
-  if [ -h ~/${1} ]; then
-    # Existing symlink 
-    echo "Removing existing symlink: ${dest}"
-    rm ${dest} 
+mkdir -p ~/.config/nvim/
+ln -s ${dotfilesDir}/.vimrc ~/.config/nvim/init.vim
 
-  elif [ -f "${dest}" ]; then
-    # Existing file
-    echo "Backing up existing file: ${dest}"
-    mv ${dest}{,.${dateStr}}
-
-  elif [ -d "${dest}" ]; then
-    # Existing dir
-    echo "Backing up existing dir: ${dest}"
-    mv ${dest}{,.${dateStr}}
-  fi
-
-  echo "Creating new symlink: ${dest}"
-  ln -s ${dotfilesDir}/${1} ${dest}
-}
-
-linkDotfile .vimrc
-linkDotfile .tmux.conf
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
