4155e0951f2a5fe589c50be90a5c4afd41ed3d5f
Krisztian Harmat <krisztian.harmat@gmail.com>
2025-04-05 08:10:41 +0200

    common lsp functionality

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,14 +1,36 @@
 return {
     'neovim/nvim-lspconfig',
     config = function()
-        require'lspconfig'.gopls.setup{}
-        require'lspconfig'.pyright.setup{}
-        -- require'lspconfig'.jdtls.setup{
-        --     on_attach = function()
-        --         vim.keymap.set('n', '<leader>n', vim.lsp.buf.rename, {buffer=0})
-        --         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
-        --     end,
-        -- }
+        local on_attach = function(client, bufnr)
+            local opts = { noremap=true, silent=true, buffer=bufnr }
 
+            -- Go to definition
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)        
+
+            -- Find references
+            vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)        
+
+            -- Go to implementation
+            vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)    
+
+            -- Rename symbol
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)            
+
+            -- Code actions
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)       
+
+            -- Hover documentation
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)                      
+
+        end        
+        require'lspconfig'.gopls.setup{
+            on_attach = on_attach,
+        }
+        require'lspconfig'.pyright.setup{
+            on_attach = on_attach,
+        }
+        require'lspconfig'.clangd.setup{
+            on_attach = on_attach,
+        }
     end,
 }
