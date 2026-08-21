610a1fe3f2ee4a69ec60376d85b5cd7ba2d70fce
Krisztian Harmat <krisztian.harmat@gmail.com>
2026-08-21 23:33:17 +0200

    fix issue in treesitter

--- private_dot_config/private_nvim/private_lua/private_plugins/private_treesitter.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/private_treesitter.lua
@@ -1,84 +1,53 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main",
+  branch = "master",
   main = "nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    branch = "main",
+    branch = "master",
   },
-  init = function()
-    local ensure = {
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "python",
-      "javascript",
-      "typescript",
-      "html",
-      "css",
-      "json",
-      "bash",
-      "markdown",
-      "markdown_inline",
-    }
-    local ok, installed = pcall(function()
-      return require("nvim-treesitter.config").get_installed()
-    end)
-    if not ok then
-      installed = {}
-    end
-    local to_install = vim.iter(ensure):filter(function(p)
-      return not vim.list_contains(installed, p)
-    end):totable()
-    if #to_install > 0 then
-      require("nvim-treesitter").install(to_install)
-    end
-
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function()
-        pcall(vim.treesitter.start)
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
   config = function()
-    require("nvim-treesitter-textobjects").setup({
-      select = {
-        lookahead = true,
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "python",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "bash",
+        "markdown",
+        "markdown_inline",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["aC"] = "@class.outer",
+            ["iC"] = "@class.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["aA"] = "@parameter.outer",
+            ["iA"] = "@parameter.inner",
+          },
+        },
       },
     })
-
-    vim.keymap.set({ "x", "o" }, "af", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@function.outer")
-    end)
-    vim.keymap.set({ "x", "o" }, "if", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@function.inner")
-    end)
-    vim.keymap.set({ "x", "o" }, "aC", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@class.outer")
-    end)
-    vim.keymap.set({ "x", "o" }, "iC", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@class.inner")
-    end)
-    vim.keymap.set({ "x", "o" }, "al", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@loop.outer")
-    end)
-    vim.keymap.set({ "x", "o" }, "il", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@loop.inner")
-    end)
-    vim.keymap.set({ "x", "o" }, "ac", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@conditional.outer")
-    end)
-    vim.keymap.set({ "x", "o" }, "ic", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@conditional.inner")
-    end)
-    vim.keymap.set({ "x", "o" }, "aA", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer")
-    end)
-    vim.keymap.set({ "x", "o" }, "iA", function()
-      require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner")
-    end)
   end,
 }
